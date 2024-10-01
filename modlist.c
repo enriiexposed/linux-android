c88dd5551e48e7c0b51b046cf14ef862fe0245c7
enririos <146124333+enririos@users.noreply.github.com>
2024-10-01 18:47:53 +0200

    Fix de errores
    Fix de errores varios importantes, el módulo compila pero falta comprobación sobre el funcionamiento cuando se inserta sobre la MV

--- modlist.c
+++ modlist.c
@@ -1,8 +1,9 @@
 #include <linux/module.h> /* Requerido por todos los módulos */
 #include <linux/kernel.h> /* Definición de KERN_INFO */
-#include <linux/kmalloc.h>
+#include <linux/slab.h>
 #include <linux/proc_fs.h>
 #include <linux/list.h>
+#include <linux/printk.h>
 
 MODULE_LICENSE("GPL"); /* Licencia del módulo */
 MODULE_DESCRIPTION("Módulo del Kernel para insertar numeros en una lista");
@@ -12,10 +13,9 @@ MODULE_AUTHOR("Alejandro Orgaz Fernández");
 // Defines de constantes del módulo
 #define MAX_SIZE 128 
 // Pendiente porque aquí no hay un puntero
-struct proc_dir_entry *proc_entry /*Información acerca del archivo creado en /proc*/
+static struct proc_dir_entry *proc_entry; /*Información acerca del archivo creado en /proc*/
 
 struct list_head numlist; /* Nodo fantasma (cabecera) de la lista enlazada */
-int nelems = 0; // numero de elementos que tenemos en la lista actualmente
 
 /* Estructura que representa los nodos de la lista */
 struct list_item {
@@ -24,10 +24,6 @@ struct list_item {
 };
 
 /* Bindeamos las funcioens que hemos creado con las que aparecen en la interfaz del kernel */
-struct proc_ops numlist_ops = {
-    .proc_read = read_numlist, //read()
-    .proc_write = write_numlist, //write()
-};
 
 /* Implementación de las funciones de lectura, escritura y clear del módulo */
 
@@ -43,7 +39,7 @@ static ssize_t read_numlist (struct file *filp, char __user *buf, size_t len, lo
     char bufaux[MAX_SIZE] = "";
     char *ptrbufaux = bufaux;
 
-    list_for_each(i, numlist) {
+    list_for_each(i, &numlist) {
         // Usamos list_entry para saber el nodo que toca
         item = list_entry(i, struct list_item, links);
         
@@ -66,7 +62,7 @@ static ssize_t read_numlist (struct file *filp, char __user *buf, size_t len, lo
         return -ENOMEM;
     }
 
-    if (copy_to_user(buf, bufaux, bytes_written) {
+    if (copy_to_user(buf, bufaux, bytes_written)) {
         return -EINVAL;
     }
 
@@ -80,12 +76,10 @@ static ssize_t write_numlist (struct file *filp, const char __user *buf, size_t 
 
     // numero que insertaremos en la lista
     int n; 
-    char buf[MAXBUF];
-
-    if (copy_from_user(buf, bufaux, len + 1) == 1) {
-        return -ENOSPC;
+    char bufaux[MAX_SIZE];
+    if (copy_from_user(bufaux, buf, len + 1) == 1) {
+        return -EFAULT;
     }
-
     bufaux[len] = '\0';
 
     // guardo un puntero del elemento borrado
@@ -94,43 +88,60 @@ static ssize_t write_numlist (struct file *filp, const char __user *buf, size_t 
     // guardo un puntero del iterador de la lista
     struct list_head *iterator = NULL;
 
+    // necesario cuando queremos hacer el kfree del puntero del elemento
+    struct list_item *number;
+
     if (sscanf(bufaux, "add %i", &n) == 1) {
-        struct list_item *newelem = (list_item*) kmalloc(sizeof(struct list_item), GFP_KERNEL);
+        struct list_item *newelem = (struct list_item*) kmalloc(sizeof(struct list_item), GFP_KERNEL);
         newelem->data = n;
-        list_add_tail(&(newelem->links), numlist);
-    } else if (sscanf(bufaux, "remove %i", &n) == 1) {
+        list_add_tail(&(newelem->links), &numlist);
+    } 
+
+    else if (sscanf(bufaux, "remove %i", &n) == 1) {
         // recorro la lista de números
-        list_for_each_safe(iterator, elemborrado, numlist) {
-            if (iterator->data == n) {
-                list.del(iterator);
-                kfree(&iterator->links);
+        list_for_each_safe(iterator, elemborrado, &numlist) {
+            number = list_entry(iterator, struct list_item, links);
+            if (number->data == n) {
+                number = list_entry(iterator, struct list_item, links);
+                list_del(iterator);
+                kfree(number);
                 printk(KERN_INFO "El elemento %d ha sido borrado\n", n);
             }
         }
-    } else if (strcmp(bufaux, "cleanup\n") == 0){
-        list_for_each_safe(iterator, elemborrado, numlist) {
-            list.del(iterator);
-            kfree(&iterator->links);
+    } 
+
+    else if (strcmp(bufaux, "cleanup\n") == 0){
+        list_for_each_safe(iterator, elemborrado, &numlist) {
+            number = list_entry(iterator, struct list_item, links);
+            list_del(iterator);
+            kfree(number);
         }
         printk(KERN_INFO "Todos los elementos han sido borrados\n");
-    } else return -EINVAL; // pendiente cambio al error adecuado
+    } 
+
+    else 
+        return -EINVAL; // pendiente cambio al error adecuado
+    
+    *off += len;
+    // En caso de exito retorna cero
     return 0;
 }   
 
-
+struct proc_ops numlist_ops = {
+    .proc_read = read_numlist, //read()
+    .proc_write = write_numlist, //write()
+};
 
 /* Función que se invoca cuando se carga el módulo en el kernel */
 int modlist_init(void) {
     // REVISAR
     INIT_LIST_HEAD(&numlist);
-    // No sé si es necesario
-    LIST_NAME("modlist");
 
     proc_entry = proc_create ("modlist", 0666, NULL, &numlist_ops);
 
     if (proc_entry == NULL) {
-        ret = -ENOMEM;
         printk(KERN_INFO "Can't create the proc entry\n");
+        return -ENOMEM;
     }
 
     printk(KERN_INFO "Modulo cargado correctamente.\n");
@@ -143,6 +154,7 @@ void modlist_clean(void) {
     remove_proc_entry("modlist", NULL);
     printk(KERN_INFO "Modulo LIN descargado. Adios kernel.\n");
 }
+
 
 
 /* Declaración de funciones init y cleanup */
