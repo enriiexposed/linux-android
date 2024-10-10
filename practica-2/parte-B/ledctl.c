a78729e16eb1dfba42072eba137316f6fd46cc7b
enririos <enririos@ucm.es>
2024-10-10 17:15:54 +0200

    Ficheros de la practica 2 realizados en el lab (10/10/2024)

---
+++ practica-2/parte-B/ledctl.c
@@ -0,0 +1,31 @@
+#include <linux/syscalls.h> /* For SYSCALL_DEFINEi() */
+#include <linux/kernel.h>
+#include <asm-generic/errno.h>
+#include <linux/module.h>
+#include <linux/tty.h>
+
+/*
+Falta meter registro en la tabla de arquitectura CUANDO COMPILEMOS KERNEL
+*/
+
+struct tty_driver* kbd_driver = NULL;
+
+SYSCALL_DEFINE1(ledctl, unisgned int leds)
+{
+  if (leds < 0 || leds > 7) {
+    printk(KERN_INFO "La mascara de bits solo admite numeros entre 0 y 7");
+    return -EINVAL;
+  }
+
+
+
+  return 0;
+}
+
+void init_driver() {
+  kbd_driver = vc_cons[fg_console].d->port.tty->driver;
+}
+
+static inline  int set_leds(struct tty_driver* handler, unsigned int mask) {
+  return (handler->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,mask);
+}
