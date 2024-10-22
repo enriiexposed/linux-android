2ae4a339f9a3fcfca5da020ef345e591573abdd8
enririos <enririos@ucm.es>
2024-10-22 14:14:57 +0200

    Arreglo del fichero ledctl

--- practica-2/parte-B/ledctl_invoke.c
+++ practica-2/parte-B/ledctl_invoke.c
@@ -8,23 +8,35 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        print_usage(argv[0]);
+        perror("Argumentos Incorrectos\n");
         return EXIT_FAILURE;
     }
 
-    char* endptr;
-    long int leds_mask = strtol(argv[1], &endptr, 10);
+    errno = 0;
 
-    if (endptr != "\0") {
+    char* cad;
+
+    long int leds_mask = strtol(argv[1], &cad, 10);
+
+    if (*cad != '\0') {
+        perror("parametro no valido");
         return 1;
     }
+
+    if (errno == EINVAL || errno == ERANGE) {
+        perror("Ha habido un error\n");
+        return -1;
+    }
+
+    errno = 0;
+
     long int res = syscall(SYS_ledctl, leds_mask);
 
     if (res == -1) {
-        perror("Error en la llamada ledctl");
+        perror("Error en la llamada ledctl\n");
         return EXIT_FAILURE;
+    } else {
+        printf("LEDs cambiados correctamente\n");
+        return 0;
     }
-
-    printf("LEDs actualizados correctamente.\n");
-    return EXIT_SUCCESS;
 }
