ce3366924c4ec922e131e702e52d1efc14ce292b
enririos <enririos@ucm.es>
2024-11-07 15:23:14 +0100

    Fin de la practica 3 y comienzo de la practica 4

--- practica-4/Clipboard-update/clipboard-update.c
+++ practica-4/Clipboard-update/clipboard-update.c
@@ -5,6 +5,8 @@
 #include <linux/string.h>
 #include <linux/vmalloc.h>
 #include <linux/uaccess.h>
+#include <linux/jiffies.h>
+#include <linux/wait.h>
 #include <linux/version.h>
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,1)
 #define __cconst__ const
@@ -13,13 +15,12 @@
 #endif
 
 
-
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Clipboard-Dev Kernel Module - FDI-UCM");
+MODULE_DESCRIPTION("Clipboard-update Kernel Module - FDI-UCM");
 MODULE_AUTHOR("Juan Carlos Saez");
 
 #define BUFFER_LENGTH       PAGE_SIZE
-#define DEVICE_NAME "clipboard" /* Dev name as it appears in /proc/devices   */
+#define DEVICE_NAME "clipboard_update" /* Dev name as it appears in /proc/devices   */
 #define CLASS_NAME "clip"
 
 
@@ -32,6 +33,13 @@ static struct cdev* chardev = NULL;
 static struct class* class = NULL;
 static struct device* device = NULL;
 static char *clipboard;  // Space for the "clipboard"
+
+/* Workqueue descriptor */
+static struct wait_queue_head my_waitq;
+
+/* Timestamp  (tick resolution) to keep track of last time the clipboard was updated */
+static unsigned long last_clipboard_update = 0;
+
 
 static ssize_t clipboard_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
   int available_space = BUFFER_LENGTH - 1;
@@ -51,15 +59,39 @@ static ssize_t clipboard_write(struct file *filp, const char __user *buf, size_t
   clipboard[len] = '\0'; /* Add the `\0' */
   *off += len;          /* Update the file position indicator */
 
+
+  /* Register timestamp */
+  last_clipboard_update = jiffies;
+
+  /* Wakeup all processes waiting for update */
+  wake_up_all(&my_waitq);
+
   return len;
 }
 
 static ssize_t clipboard_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
 
   int nr_bytes;
+  unsigned long last_update_local;
 
   if ((*off) > 0) /* Tell the application that there is nothing left to read */
     return 0;
+
+  /* Increment this module's reference counter */
+  try_module_get(THIS_MODULE);
+
+  /* Wait until next clipboard update */
+  last_update_local = last_clipboard_update;
+
+  if (wait_event_interruptible(my_waitq ,
+                               time_after(last_clipboard_update, last_update_local)) ) {
+    pr_info("Blocking operation interrupted due to signal\n");
+    module_put(THIS_MODULE);
+    return -EINTR;
+  }
+
+  /* Decrement this module's reference counter */
+  module_put(THIS_MODULE);
 
   nr_bytes = strlen(clipboard);
 
@@ -70,7 +102,7 @@ static ssize_t clipboard_read(struct file *filp, char __user *buf, size_t len, l
   if (copy_to_user(buf, clipboard, nr_bytes))
     return -EINVAL;
 
-  (*off) += len; /* Update the file position indicator */
+  (*off) += len; /* Update the file pointer */
 
   return nr_bytes;
 }
@@ -98,10 +130,15 @@ int init_clipboard_module( void )
 
   clipboard = (char *)vmalloc( BUFFER_LENGTH );
 
-  if (!clipboard)
+  if (!clipboard) {
+    printk(KERN_INFO "Can't allocate clipboard memory");
     return  -ENOMEM;
+  }
 
   memset( clipboard, 0, BUFFER_LENGTH );
+
+  /* Init wait queue */
+  init_waitqueue_head(&my_waitq);
 
   /* Get available (major,minor) range */
   if ((ret = alloc_chrdev_region (&start, 0, 1, DEVICE_NAME))) {
@@ -135,7 +172,7 @@ int init_clipboard_module( void )
   /* Establish function that will take care of setting up permissions for device file */
   class->devnode = custom_devnode;
 
-  /* Creating device */
+  /*Creating device*/
   device = device_create(class, NULL, start, NULL, DEVICE_NAME);
 
   if (IS_ERR(device)) {
@@ -151,7 +188,7 @@ int init_clipboard_module( void )
   printk(KERN_INFO "the driver try to cat and echo to /dev/%s.\n", DEVICE_NAME);
   printk(KERN_INFO "Remove the module when done.\n");
 
-  printk(KERN_INFO "Clipboard-dev: Module loaded.\n");
+  printk(KERN_INFO "Clipboard-update: Module loaded.\n");
 
   return 0;
 
@@ -179,7 +216,7 @@ error_alloc_region:
 void exit_clipboard_module( void )
 {
   if (device)
-    device_destroy(class, device->devt);
+    device_unregister(device);
 
   if (class)
     class_destroy(class);
@@ -195,7 +232,7 @@ void exit_clipboard_module( void )
 
   vfree(clipboard);
 
-  printk(KERN_INFO "Clipboard-dev: Module unloaded.\n");
+  printk(KERN_INFO "Clipboard-update: Module unloaded.\n");
 }
 
 
