f1ca6b3da0b6723ac6fea1cecdbc4dd885d7f9da
Enrique Ríos <146124333+enririos@users.noreply.github.com>
2024-11-12 18:59:42 +0100

    Fin de la parte B de la practica 4
    Implementado y testado el funcionamiento de la parte B de la practica 4

--- practica-4/ProdCons/prodcons.c
+++ practica-4/ProdCons/prodcons.c
@@ -1,18 +1,27 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>
+#include <linux/kfifo.h>
+#include <linux/miscdevice.h>
 #include <linux/semaphore.h>
+
+
+MODULE_DESCRIPTION("Prodcons Kernel Module - LIN FDI-UCM");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Enrique Rios Rios");
+MODULE_AUTHOR("Alejandro Orgaz");
 
 /* Defines necesarios para el modulo */
 #define MAX_BUF_ELEMS 8
 #define MAX_CHARS_AUX_BUF MAX_BUF_ELEMS * 2
+#define DEVICE_NAME "prodcons"  /* Dev name as it appears in /proc/devices   */
 /* Cabeceras de funciones usadas */
-int __init__ prodcons_init(void);
-void __exit__ prodcons_exit(void);
-int prodcons_open (struct inode *, struct file *);
-int prodcons_release (struct inode *, struct file *);
-ssize_t prodcons_read (struct file *, char __user *, size_t, loff_t *);
-ssize_t prodcons_write (struct file *, const char __user *, size_t, loff_t *);
+int prodcons_init(void);
+void prodcons_exit(void);
+static int prodcons_open (struct inode *node, struct file *filp);
+static int prodcons_release (struct inode *node, struct file *filp);
+static ssize_t prodcons_read (struct file *filp, char __user *buf, size_t len, loff_t *off);
+static ssize_t prodcons_write (struct file *filp, const char __user *buf, size_t len, loff_t *off);
 
 /* Estructuras necesarias*/
 static struct file_operations fops = {
@@ -21,28 +30,38 @@ static struct file_operations fops = {
     .release = prodcons_release,
    	.write = prodcons_write
 };
-static struct kfifo cbuf;
-/* semaforo para controlar la escritura */
-DEFINE_SEMAPHORE(mtx);
-/* semaforo para controlar la escritura */
-DEFINE_SEMAPHORE(huecos);
-/* semaforo para controlar la lectura */
-DEFINE_SEMAPHORE(elementos);
 
-int prodcons_open (struct inode *node, struct file *filp) {
+static struct miscdevice misc_prodcons = {
+    .minor = MISC_DYNAMIC_MINOR,    /* kernel dynamically assigns a free minor# */
+    .name = DEVICE_NAME, /* when misc_register() is invoked, the kernel
+                        * will auto-create device file as /dev/chardev ;
+                        * also populated within /sys/class/misc/ and /sys/devices/virtual/misc/ */
+    .mode = 0666,     /* ... dev node perms set as specified here */
+    .fops = &fops,    /* connect to this driver's 'functionality' */
+};
+
+static struct kfifo cbuf;
+
+static struct semaphore mtx;
+static struct semaphore elementos;
+static struct semaphore huecos;
+
+
+static int prodcons_open (struct inode *node, struct file *filp) {
     try_module_get(THIS_MODULE);
     return 0;
 }
 
-void prodcons_release(struct inode *node, struct file *filp) {
+static int prodcons_release(struct inode *node, struct file *filp) {
     module_put(THIS_MODULE);
+    return 0;
 }
 
 
-ssize_t prodcons_read (struct file *filp, char __user *buf, size_t len, loff_t *off) {
-    int value;
-    int nbytes;
+static ssize_t prodcons_read (struct file *filp, char __user *buf, size_t len, loff_t *off) {
+    int val;
     char auxbuf[MAX_CHARS_AUX_BUF+1];
+    ssize_t size;
 
     if ((*off) > 0) {
         return 0;
@@ -61,28 +80,37 @@ ssize_t prodcons_read (struct file *filp, char __user *buf, size_t len, loff_t *
     
     kfifo_out(&cbuf, &val, sizeof(int));
 
-    ssize_t size sprintf(auxbuf, "%i\n", val);
+    size = sprintf(auxbuf, "%i\n", val);
 
     if (copy_to_user(buf, auxbuf, size)) {
         return -ENOMEM;
     }
 
-    up(&huecos);
     up (&mtx);
+    up(&huecos);
+
+    (*off) += len;
 
     return size;
 }
 
-ssize_t prodcons_write (struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+static ssize_t prodcons_write (struct file *filp, const char __user *buf, size_t len, loff_t *off) {
     char auxbuf[MAX_CHARS_AUX_BUF + 1];
     int val = 0;
 
     if (copy_from_user(auxbuf, buf, len)) {
         return -ENOMEM;
     }
+    
     auxbuf[len] = '\0';
-    sscanf(auxbuf, "%d", &val);
+    if (sscanf(auxbuf, "%d%*s", &val) != 1) {
+        printk(KERN_INFO "Argumento incorrecto al escribir\n");
+        return -EINVAL;
+    }
 
+    printk(KERN_INFO "Numero a insertar: %d", val);
+
+    // no dejamos que otro proceso escriba en los huecos
     if (down_interruptible(&huecos)) {
         return -EINTR;
     }
@@ -92,32 +120,41 @@ ssize_t prodcons_write (struct file *filp, const char __user *buf, size_t len, l
         return -EINTR;
     }
 
-    kfifo_in(cbuf, &val, sizeof(int));
+    kfifo_in(&cbuf, &val, sizeof(int));
     
-    up(&elementos);
     up(&mtx);
+    up(&elementos);
+
+    (*off) += len;
 
     return len;
 }
 
 
 
-int __init__ prodcons_init(void) {
+int prodcons_init(void) {
 	if (kfifo_alloc(&cbuf, MAX_BUF_ELEMS*sizeof(int), GFP_KERNEL)) {
         return -ENOMEM;
 	}
 
     sema_init(&mtx, 1);
-    sema_init(&huecos, 0);
-    sema_init(&elementos, MAX_BUF_ELEMS);
+    sema_init(&huecos, MAX_BUF_ELEMS);
+    sema_init(&elementos, 0);
+
+    misc_register(&misc_prodcons);
+
+    printk(KERN_INFO "Modulo cargado correctamente\n");
 
     return 0;
 }
 
-void __exit__ prodcons_exit(void) {
-
+void prodcons_exit(void) {
+    misc_deregister(&misc_prodcons);
+    kfifo_free(&cbuf);
+    printk(KERN_INFO "Modulo descargado correctamente\n");
 }
 
 
 module_init(prodcons_init);
 module_exit(prodcons_exit);
+
