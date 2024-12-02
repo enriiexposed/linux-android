05c3697e9fbc221896814327b70ea057f86b7794
enririos <enririos@ucm.es>
2024-12-02 11:04:23 +0100

    inicio de la implementacion de las interrupciones del boton del reproductor

--- practica-5/Parte-B/buzzer.c
+++ practica-5/Parte-B/buzzer.c
@@ -10,6 +10,47 @@ MODULE_DESCRIPTION("A module that reproduces songs with raspberry's buzzer")
 
 #define DEVICE_NAME "buzzer"
 
+/* Parámetros del módulo cargable */
+static unsigned short beat = 120;
+module_param(unsigned short, beat, 0);
+MODULE_PARAM_DESC(beat, "Marca el numero de figuras negras que sonaran en un minuto");
+
+struct music_step {
+    short freq,
+    int time
+}
+
+/* Definimos la lista como un puntero a una lista que crearemos con memoria dinámica */
+static struct music_step* song;
+
+static spinlock_t lock; /* Cerrojo para proteger actualización/consulta 
+							de variables buzzer_state y buzzer_request */
+static struct music_step* next_note=NULL; /* Puntero a la siguiente nota de la melodía 
+											actual  (solo alterado por tarea diferida) */
+
+typedef enum {
+    BUZZER_STOPPED, /* Buzzer no reproduce nada (la melodía terminó o no ha comenzado) */
+    BUZZER_PAUSED,	/* Reproducción pausada por el usuario */
+    BUZZER_PLAYING	/* Buzzer reproduce actualmente la melodía */
+} buzzer_state_t;
+
+static buzzer_state_t buzzer_state=BUZZER_STOPPED; /* Estado actual de la reproducción */
+
+typedef enum {
+    REQUEST_START,		/* Usuario pulsó SW1 durante estado BUZZER_STOPPED */
+    REQUEST_RESUME,		/* Usuario pulsó SW1 durante estado BUZZER_PAUSED */
+    REQUEST_PAUSE,		/* Usuario pulsó SW1 durante estado BUZZER_PLAYING */
+    REQUEST_CONFIG,		/* Usuario está configurando actualmente una nueva melodía vía /dev/buzzer  */
+    REQUEST_NONE			/* Indicador de petición ya gestionada (a establecer por tarea diferida) */
+} buzzer_request_t;
+
+static buzzer_request_t buzzer_request=REQUEST_NONE;
+
+/* Variables globales para la interrupcion del boton*/
+#define GPIO_BUTTON 22
+struct gpio_desc* desc_button = NULL;
+static int gpio_button_irqn = -1;
+
 static int buzzer_open(struct inode *inode, struct file *file);
 static int buzzer_release(struct inode *inode, struct file *file);
 static ssize_t buzzer_read(struct file *filp, char *buf, size_t len, loff_t *off);
@@ -28,6 +69,22 @@ static struct miscdevice misc = {
     .mode = 0666,
     .fops = &fops,
 };
+
+/* Interrupt handler for button **/
+static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
+{
+#ifdef MANUAL_DEBOUNCE
+  static unsigned long last_interrupt = 0;
+  unsigned long diff = jiffies - last_interrupt;
+  if (diff < 20)
+    return IRQ_HANDLED;
+
+  last_interrupt = jiffies;
+#endif
+
+  return IRQ_HANDLED;
+}
+
 
 #define Max_BEAT_LENGTH = 20;
 
@@ -53,12 +110,46 @@ static ssize_t buzzer_read(struct file *filp, char *buf, size_t len, loff_t *off
 }
 
 static int __init buzzer_init(void) {
+    char gpio_out_ok = 0;
     misc_register(&misc);
+    /* Requesting Button's GPIO */
+    if ((err = gpio_request(GPIO_BUTTON, "button"))) {
+        pr_err("ERROR: GPIO %d request\n", GPIO_BUTTON);
+        goto err_handle;
+    }
+
+    gpio_out_ok = 1;
+
+    //Get the IRQ number for our GPIO
+    gpio_button_irqn = gpiod_to_irq(desc_button);
+    pr_info("IRQ Number = %d\n", gpio_button_irqn);
+
+    if (request_irq(gpio_button_irqn,             //IRQ number
+                    gpio_irq_handler,   //IRQ handler
+                    IRQF_TRIGGER_RISING,        //Handler will be called in raising edge
+                    "button_leds",               //used to identify the device name using this IRQ
+                    NULL)) {                    //device id for shared IRQ
+        pr_err("my_device: cannot register IRQ ");
+        goto err_handle;
+    }
+
+    try_module_get(THIS_MODULE);
+
+    return 0;
+    
+err_handle:
+    if (gpio_out_ok)
+        gpiod_put(desc_button);
+
+return err;    
 }
 
 
 static void __exit buzzer_exit(void) {
     misc_deregister(&misc);
+    free_irq(gpio_button_irqn, NULL);
+    gpiod_put(desc_button);
+    module_put(THIS_MODULE);
 }
 
 
